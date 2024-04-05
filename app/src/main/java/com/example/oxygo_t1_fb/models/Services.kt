47fe928fdced1b0a1c7255baf9bcea2cf4852ae2
Dani Renteria <Drenteria@oxy.edu>
2024-04-05 15:12:22 -0700

    Added service data class

---
+++ app/src/main/java/com/example/oxygo_t1_fb/models/Services.kt
@@ -0,0 +1,11 @@
+package com.example.oxygo_t1_fb.models
+
+data class Services(
+
+    val id: String? = null,
+    val name: String? = null,
+    val description: String? = null,
+    val cost: Int? = null,
+    val Availability: String? = null
+
+)
